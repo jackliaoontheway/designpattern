3daa8193705bd54cab9a3f9d44029d91c3e7c3a3
jackliao <ldhontheway@live.com>
2019-07-27 15:15:41 +0800

    prototype 深拷贝通过 ObjectOutputStream

--- designpattern/src/creational/prototype/GraphicDesignPatterns/ConcreteProduct.java
+++ designpattern/src/creational/prototype/GraphicDesignPatterns/ConcreteProduct.java
@@ -1,5 +1,11 @@
 package creational.prototype.GraphicDesignPatterns;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class ConcreteProduct implements Product {
 
 	private String name;
@@ -53,5 +59,24 @@ public class ConcreteProduct implements Product {
 		concreteProduct.setSubProduct((SubProduct)subProduct.clone());
 		return concreteProduct;
 	}
+	
+	/**
+	 * 深拷贝通过 ObjectOutputStream
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+    public Object deepClone() throws IOException, ClassNotFoundException {  
+  
+        /* 写入当前对象的二进制流 */  
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();  
+        ObjectOutputStream oos = new ObjectOutputStream(bos);  
+        oos.writeObject(this);  
+  
+        /* 读出二进制流产生的新对象 */  
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());  
+        ObjectInputStream ois = new ObjectInputStream(bis);  
+        return ois.readObject();  
+    }
 	
 }

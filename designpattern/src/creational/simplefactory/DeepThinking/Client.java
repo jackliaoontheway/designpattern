62b9bbcba948c1146502021696b35a3e09924168
jack <jack>
2019-08-12 10:54:12 +0800

    Test Linux idea commit

--- designpattern/src/creational/simplefactory/DeepThinking/Client.java
+++ designpattern/src/creational/simplefactory/DeepThinking/Client.java
@@ -3,7 +3,11 @@ package creational.simplefactory.DeepThinking;
 public class Client {
 
     public static void main(String[] args) {
+
         Api api = Factory.createApi();
+
+        api.operation();
+
     }
 
 }
